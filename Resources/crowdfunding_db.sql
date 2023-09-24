d05837ff400e23a09da88fdab1e820619b20d0eb
lydiab72 <132516418+lydiab72@users.noreply.github.com>
2023-09-24 15:48:11 -0600

    Add files via upload

--- Resources/crowdfunding_db.sql
+++ Resources/crowdfunding_db.sql
@@ -1,23 +1,3 @@
-CREATE TABLE "contacts" (
-    "contact_id" INT NOT NULL,
-    "first_name" VARCHAR NOT NULL,
-    "last_name" VARCHAR NOT NULL,
-    "email" VARCHAR NOT NULL,
-    PRIMARY KEY ("contact_id")
-);
-
-CREATE TABLE "category" (
-    "category_id" VARCHAR NOT NULL,
-    "category" VARCHAR NOT NULL,
-    PRIMARY KEY ("category_id")
-);
-
-CREATE TABLE "subcategory" (
-    "subcategory_id" VARCHAR NOT NULL,
-    "subcategory" VARCHAR NOT NULL,
-    PRIMARY KEY ("subcategory_id")
-);
-
 CREATE TABLE "campaign" (
     "cf_id" INT PRIMARY KEY NOT NULL,
     "contact_id" INT NOT NULL,
@@ -36,6 +16,26 @@ CREATE TABLE "campaign" (
 	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
     FOREIGN KEY (category_id) REFERENCES category(category_id),
     FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+CREATE TABLE "contacts" (
+    "contact_id" INT NOT NULL,
+    "first_name" VARCHAR NOT NULL,
+    "last_name" VARCHAR NOT NULL,
+    "email" VARCHAR NOT NULL,
+    PRIMARY KEY ("contact_id")
+);
+
+CREATE TABLE "category" (
+    "category_id" VARCHAR NOT NULL,
+    "category" VARCHAR NOT NULL,
+    PRIMARY KEY ("category_id")
+);
+
+CREATE TABLE "subcategory" (
+    "subcategory_id" VARCHAR NOT NULL,
+    "subcategory" VARCHAR NOT NULL,
+    PRIMARY KEY ("subcategory_id")
 );
 
 select * from contacts;
